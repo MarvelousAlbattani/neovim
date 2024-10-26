ea60899dbbfb1dd3e3435e9c752a412adfbc80a9
luca <baccinelliluca@gmail.com>
2024-10-26 10:24:48 +0200

    fix none-ls setup

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,18 +1,23 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.prettier,
-					null_ls.builtins.diagnostics.eslint_d,
-					null_ls.builtins.completion.spell,
-				},
-			})
+    {
+        "nvimtools/none-ls.nvim",
+        dependencies = {
+            "nvimtools/none-ls-extras.nvim",
+        },
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.prettier,
+                    null_ls.builtins.completion.spell,
+                    require("none-ls.diagnostics.eslint"),
+                    require("none-ls.formatting.jq"),
+                    require("none-ls.code_actions.eslint"),
+                },
+            })
 
-			vim.keymap.set("n", "<C-f>", vim.lsp.buf.format, {})
-		end,
-	},
+            vim.keymap.set("n", "<C-f>", vim.lsp.buf.format, {})
+        end,
+    },
 }
