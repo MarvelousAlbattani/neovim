c92570c0fe27a4f6df37db0044833b7189bd8809
luca <baccinelliluca@gmail.com>
2024-11-13 00:24:36 +0100

    removed transparent_background catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,8 +1,14 @@
-return { 
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme "catppuccin"
-    end
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+	    vim.cmd.colorscheme("catppuccin")
+
+		local catppuccin = require("catppuccin")
+
+		catppuccin.setup({
+			transparent_background = false,
+		})
+	end,
 }
