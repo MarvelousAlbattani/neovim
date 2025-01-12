b70b6605706b1356e1d263c8a07e3a24340d2665
luca <baccinelliluca@gmail.com>
2025-01-13 00:43:13 +0100

    added keybind to show every command from plugins

--- init.lua
+++ init.lua
@@ -56,3 +56,7 @@ end
 
 -- without this, pressing n will select everything
 vim.keymap.set('n', 'n', function() end, { desc = "avoid selecting everything" })
+-- show all the commands from plugins
+vim.keymap.set('n', '<leader>c', function()
+    vim.cmd("Telescope commands")
+end, { desc = "Show all commands" })
