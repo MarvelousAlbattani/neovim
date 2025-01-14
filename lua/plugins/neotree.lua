b69a6411de8cee080e9ef45790f536a3286748f1
luca <baccinelliluca@gmail.com>
2025-01-14 01:05:39 +0100

    edit neotree icons

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -28,11 +28,10 @@ return {
 				},
 				git_status = {
 					symbols = {
-						added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-						modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-						deleted = "✖", -- this can only be used in the git_status source
-						renamed = "󰁕", -- this can only be used in the git_status source
-						-- Status type
+						added = "✚",
+						modified = "",
+						deleted = "✖",
+						renamed = "󰁕",
 						untracked = "",
 						ignored = "",
 						unstaged = "󰄱",
