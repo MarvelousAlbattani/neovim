33f421f5ad8b72bd52a31435f57554b4336a0da2
luca <baccinelliluca@gmail.com>
2025-01-16 21:40:28 +0100

    fix neotree reveal removed when query result appear

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -36,27 +36,30 @@ require("lazy").setup("plugins")
 
 -- Open neotree by default
 vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		require("neo-tree.command").execute({})
-	end,
+    callback = function()
+        require("neo-tree.command").execute({})
+    end,
 })
 
 -- Reveal file in neotree when opened
 vim.api.nvim_create_autocmd("BufReadPost", {
     pattern = "*",
     callback = function()
-        vim.cmd('Neotree reveal')
-    end
+        -- avoid showing neotree when query result appears
+        if vim.bo.filetype ~= "" and vim.bo.filetype ~= "dbout" then
+            vim.cmd("Neotree reveal")
+        end
+    end,
 })
 
 local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
 if not vim.loop.fs_stat(pipepath) then
-	vim.fn.serverstart(pipepath)
+    vim.fn.serverstart(pipepath)
 end
 
 -- without this, pressing n will select everything
-vim.keymap.set('n', 'n', function() end, { desc = "avoid selecting everything" })
+vim.keymap.set("n", "n", function() end, { desc = "avoid selecting everything" })
 -- show all the commands from plugins
-vim.keymap.set('n', '<leader>c', function()
+vim.keymap.set("n", "<leader>c", function()
     vim.cmd("Telescope commands")
 end, { desc = "Show all commands" })
