05588a9c0d8e1d80d3606f259e959178a0578465
luca <baccinelliluca@gmail.com>
2025-10-12 15:09:59 +0200

    Changed bufferline conf

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,9 +2,17 @@ return {
     {
         'akinsho/bufferline.nvim',
         version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons',
+        dependencies = { 
+            'nvim-tree/nvim-web-devicons',
+            'ThePrimeagen/harpoon',
+        },
         config = function()
             require("bufferline").setup({})
+            local harpoon = require("harpoon")
+            for i, item in ipairs(harpoon:list().items) do
+                -- added harpoon saved to buffer list showed by bufferline
+                vim.cmd("badd " .. vim.fn.fnameescape(item.value))
+            end
         end
     }
 }
